20095ccd20fcc987dc2b1a9142ffb57fc3d72b6d
Junior <kazunguderiq@gmail.com>
2022-10-11 17:31:54 +0300

    work on methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "Naureen")
+  if name.length > 0
+    puts "Hello, #{name}!"
+  end
+  puts "Hello, programmer!"
+end
+
+def add(a, b)
+  return a + b
+end
+
+def halve(number)
+  if number.class != Integer
+    return nil
+  end
+  return (number) / 2
+end
